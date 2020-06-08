05f24365869d74a7f14497610bc254cfdc2ef3ae
felix <felix.lundh@gmail.com>
2020-06-08 15:52:09 +0200

    done
    checks if there has been new matches played and updates the stats only if there has been new matches played

--- Player.py
+++ Player.py
@@ -1,4 +1,4 @@
-# This class represents a player. You instanciate a player and then add the relevant stats to the player
+# This class represents a player. You instantiate a player and then add the relevant stats to the player
 
 
 class Player:
@@ -6,23 +6,21 @@ class Player:
         self.name = name
         self.iD = iD
         self.matchesURL = matchesURL
+        self.matches = []
+        self.matchURLSTART = ""
+        self.matchURLMID = "id="
+        self.matchURLEND = ""
+        self.kdr = 0
+        self.clutches = 0
+        self.openingKills = 0
+        self.hSP = 0
+        self.assists = 0
+        self.eloChange = 0
+        self.mostKillsInOneMatch = 0
+        self.defuses = 0
+        self.plants = 0
 
     def printStats(self):
         print(self.name)
         print("Kdr: ", self.kdr, "Clutches: ", self.clutches, "Opening kills: ", self.openingKills, "Hs%: ",
               self.hSP, '%', "Assists: ", self.assists, "Elo change: ", self.eloChange)
-
-    matches = []
-    matchURLSTART = ""
-    matchURLMID = "id="
-    matchURLEND = ""
-
-    kdr = 0
-    clutches = 0
-    openingKills = 0
-    hSP = 0
-    assists = 0
-    eloChange = 0
-    mostKillsInOneMatch = 0
-    defuses = 0
-    plants = 0
